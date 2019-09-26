fef06dea59ee9b1c9f1f72bac2435675fd1bc825
Simpan1001 <simon.springer@edu.huddinge.se>
2019-09-26 11:52:14 +0200

    Add function complete

--- WindowCalculator/Form1.cs
+++ WindowCalculator/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,35 @@ namespace WindowCalculator
 {
     public partial class Form1 : Form
     {
+        ArrayList Numbers = new ArrayList();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Number t = new Number();
+            t.number1 = textBox1.Text;
+            t.number2 = textBox2.Text;
+
+            string d = "";
+            int a, b, c = 0;
+
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a + b;
+            d = Convert.ToString(c);
+            t.stringRes = textBox1.Text + " + " + textBox2.Text + " = " + d;
+
+            Numbers.Add(t.stringRes);
+            listBox1.Items.Add(t.stringRes);
+        }
+    }
+    class Number {
+        public string number1 { get; set; }
+        public string number2 { get; set; }
+        public string result { get; set; }
+        public string stringRes { get; set; }
     }
 }
